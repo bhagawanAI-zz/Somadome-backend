4e77de295510bb92e027ba8b210d6762f3e59b35
parag badkul <pbadkul786@gmail.com>
2022-01-26 23:41:05 +0530

    adding code to reset token

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
 from app import app
 from db import db
-from ma import ma 
+from ma import ma
 
-db.init_app(app)
-ma.init_app(app)
-app.run(port=5000)
 
 @app.before_first_request
 def create_tables():
     db.create_all()
+
+
+if __name__ == "__main__":
+    db.init_app(app)
+    ma.init_app(app)
+    app.run(port=5000)
