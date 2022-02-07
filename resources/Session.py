7eb4904eeed3599abdb3d37289a62b6355738777
parag badkul <pbadkul786@gmail.com>
2022-02-07 22:05:23 +0530

    get session

--- resources/Session.py
+++ resources/Session.py
@@ -1,8 +1,8 @@
-import json
+# import json
 
 from flask import jsonify
 from flask_restful import Resource, reqparse
-import pandas as pd
+# import pandas as pd
 from marshmallow import ValidationError
 
 from models.Session import SessionModel
